27ca4f44a32e7263440c0d4d03a5aa71617fe30f
Bruna Fernanda <brunafernanda.mts@gmail.com>
2023-05-04 17:44:55 -0300

    correção

--- trabalho faculdade/painel.php
+++ trabalho faculdade/painel.php
@@ -1,5 +1,14 @@
 <?php
-include('valida_login.php');
+
+if(!isset($_SESSION)) {
+    session_start();
+}
+
+if(!isset($_SESSION['id'])) {
+    die("Você não pode acessar esta página porque não está logado.<p><a href=\"index.php\">Entrar</a></p>");
+}
+
+
 ?>
 
 <!DOCTYPE html>
