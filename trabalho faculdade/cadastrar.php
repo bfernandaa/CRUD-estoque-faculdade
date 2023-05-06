872636ca3e94e67ba79ccfe2a19c2d1b29c8b240
Bruna Fernanda <brunafernanda.mts@gmail.com>
2023-05-06 16:08:56 -0300

    teste

--- trabalho faculdade/cadastrar.php
+++ trabalho faculdade/cadastrar.php
@@ -1,13 +1,13 @@
 <?php
 include('conexao.php');
 
-$idproduto = $_POST["idproduto"];
+// $idproduto = $_POST["idproduto"];
 $descricao = $_POST["descricao"];
 $quantidade = $_POST["quantidade"];
 $valor = $_POST["valor"];
 // $btncadastro = $_POST["btncadastro"];
 
 
-$cadastro = "INSERT INTO produtos (idproduto, descricao, quantidade, valor) VALUES ('$idproduto', '$descricao', '$quantidade', '$valor')";
+$cadastro = "INSERT INTO produtos (descricao, quantidade, valor) VALUES ('$descricao', '$quantidade', '$valor')";
 
 ?>
