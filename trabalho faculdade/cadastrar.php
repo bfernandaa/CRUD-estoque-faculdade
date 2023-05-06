822ed343eb9f56966b901b7bf26d9bb9487485a1
Bruna Fernanda <brunafernanda.mts@gmail.com>
2023-05-06 16:06:15 -0300

    teste

--- trabalho faculdade/cadastrar.php
+++ trabalho faculdade/cadastrar.php
@@ -5,7 +5,7 @@ $idproduto = $_POST["idproduto"];
 $descricao = $_POST["descricao"];
 $quantidade = $_POST["quantidade"];
 $valor = $_POST["valor"];
-$btncadastro = $_POST["btncadastro"];
+// $btncadastro = $_POST["btncadastro"];
 
 
 $cadastro = "INSERT INTO produtos (idproduto, descricao, quantidade, valor) VALUES ('$idproduto', '$descricao', '$quantidade', '$valor')";
