6bb711eb880682107186c3394126b1a6f766c420
Bruna Fernanda <brunafernanda.mts@gmail.com>
2023-05-09 00:01:16 -0300

    teste

--- trabalho faculdade/alterar.php
+++ trabalho faculdade/alterar.php
@@ -12,7 +12,7 @@ $id = $_SESSION['id'];
 
 
 
-$sql = "UPDATE produtos SET descricao=$descricao, marca=$marca, quantidade=$quantidade, valor=$valor WHERE codproduto=$codproduto";
+$sql = "UPDATE produtos SET descricao={$descricao}, marca={$marca}, quantidade={$quantidade}, valor={$valor} WHERE codproduto={$codproduto}";
 $query = $mysqli->query($sql) or die("Falha na execução do código SQL: " . $mysqli->error);
 
 header("Location: painel.php");
