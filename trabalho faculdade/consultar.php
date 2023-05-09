6bf5ab77cc9d6fdf0aa85d3e903188b6b2e6edb9
Bruna Fernanda <brunafernanda.mts@gmail.com>
2023-05-09 20:02:00 -0300

    teste

--- trabalho faculdade/consultar.php
+++ trabalho faculdade/consultar.php
@@ -15,6 +15,23 @@ $id = $_SESSION['id'];
 $sql = "SELECT codproduto, descricao, marca, quantidade, valor FROM produtos WHERE codproduto=$codproduto";
 $query = $mysqli->query($sql) or die("Falha na execução do código SQL: " . $mysqli->error);
 
+while($exibe = mysqli_fetch_row($sql)){
+
+    echo  "<tr><td>Código do Produto</td>";
+    echo "<td>".$exibe[1]."</td></tr>";
+    echo  "<tr><td>Descrição</td>";
+    echo "<td>".$exibe[2]."</td></tr>";
+    echo  "<tr><td>Marca</td>";
+    echo "<td>".$exibe[3]."</td></tr>";
+    echo  "<tr><td>Quantidade</td>";
+    echo "<td>".$exibe[4]."</td></tr>";
+    echo  "<tr><td>Valor</td>";
+    echo "<td>".$exibe[5]."</td></tr>";
+ 
+ }
+ 
+ echo "</table>";
+
 // header("Location: painel.php");
 
 ?>
