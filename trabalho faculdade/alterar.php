94effe6f578a30a44e199a893a797d46db89a489
Bruna Fernanda <brunafernanda.mts@gmail.com>
2023-05-08 23:56:45 -0300

    construção form editar

---
+++ trabalho faculdade/alterar.php
@@ -0,0 +1,20 @@
+<?php
+include('conexao.php');
+session_start();
+
+$codproduto = $_POST["codproduto"];
+$descricao = $_POST["descricao"];
+$marca = $_POST["marca"];
+$quantidade = $_POST["quantidade"];
+$valor = $_POST["valor"];
+$btn = $_POST["alteracao"];
+$id = $_SESSION['id'];
+
+
+
+$sql = "UPDATE produtos SET descricao=$descricao, marca=$marca, quantidade=$quantidade, valor=$valor WHERE codproduto=$codproduto";
+$query = $mysqli->query($sql) or die("Falha na execução do código SQL: " . $mysqli->error);
+
+header("Location: painel.php");
+
+?>
