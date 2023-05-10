cb18d925b6faabf7a1a73bf381362df98182bd39
Bruna Fernanda <brunafernanda.mts@gmail.com>
2023-05-09 22:11:11 -0300

    teste

--- trabalho faculdade/consultar.php
+++ trabalho faculdade/consultar.php
@@ -15,6 +15,8 @@ $id = $_SESSION['id'];
 $sql = "SELECT descricao, marca, quantidade, valor FROM produtos WHERE codproduto=$codproduto";
 $query = $mysqli->query($sql) or die("Falha na execução do código SQL: " . $mysqli->error);
 
+print_r($_POST);
+
 if ($result = $mysqli -> query($sql)) {
     while ($row = $result -> fetch_row()) {
       printf ("%s (%s)\n", $row[0], $row[1], $row[2], $row[3], $row[4]);
