07f1cd1435981a768eeb2021ac42b1eb918fb783
Bruna Fernanda <brunafernanda.mts@gmail.com>
2023-05-15 19:23:57 -0300

    teste

--- trabalho faculdade/consultar.php
+++ trabalho faculdade/consultar.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Estoque - Bom Preço</title>
+    <link rel="stylesheet" href="telaEstoque.css">
+</head>
+<body>
+    <header>
+        <div id="logoBomPreco">
+           <a href="painel.php"><img src="imagens/logo-mercado2.PNG" alt=""></a> 
+        </div>
+        <button id="btSair"><a href="logout.php">Sair</a></button>
+        <h1 id="titulo">Gerenciamento de Estoque</h1>
+        <h3 id="welcome">Bem-vindo(a), <?php echo $_SESSION['nome']; ?> </h3>
+    </header>
+    <section id="areaProdutos">
+        <button id="actionProdutoCadastrar" class="btn" >Cadastrar Produto</button>
+        <button id="actionProdutoConsultar" class="btn" >Consultar Produto</button>
+        <button id="actionProdutoEditar" class="btn" >Editar Produto</button>
+        <button id="actionProdutoExcluir" class="btn" >Excluir Produto</button>
+    </section>
 <?php
 include('conexao.php');
 session_start();
@@ -29,27 +53,3 @@ echo "</table>";
 // header("Location: painel.php");
 
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Controle de Estoque - Bom Preço</title>
-    <link rel="stylesheet" href="telaEstoque.css">
-</head>
-<body>
-    <header>
-        <div id="logoBomPreco">
-           <a href="painel.php"><img src="imagens/logo-mercado2.PNG" alt=""></a> 
-        </div>
-        <button id="btSair"><a href="logout.php">Sair</a></button>
-        <h1 id="titulo">Gerenciamento de Estoque</h1>
-        <h3 id="welcome">Bem-vindo(a), <?php echo $_SESSION['nome']; ?> </h3>
-    </header>
-    <section id="areaProdutos">
-        <button id="actionProdutoCadastrar" class="btn" >Cadastrar Produto</button>
-        <button id="actionProdutoConsultar" class="btn" >Consultar Produto</button>
-        <button id="actionProdutoEditar" class="btn" >Editar Produto</button>
-        <button id="actionProdutoExcluir" class="btn" >Excluir Produto</button>
-    </section>
