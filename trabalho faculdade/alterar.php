e318bb584dc8b6d4cc4010bb67e6304923439600
Bruna Fernanda <brunafernanda.mts@gmail.com>
2023-05-15 22:24:57 -0300

    teste

--- trabalho faculdade/alterar.php
+++ trabalho faculdade/alterar.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Estoque - Bom Preço</title>
+    <link rel="stylesheet" href="telaEstoque.css">
+</head>
+<body>
+    <header>
+        <div id="logoBomPreco">
+           <a href="painel.php"><img src="imagens/logo-mercado2.PNG" alt=""></a> 
+        </div>
+        <a href="logout.php" id="btSair">Sair</a>
+        <h1 id="titulo">Gerenciamento de Estoque</h1>
+    </header>
 <?php
 include('conexao.php');
 session_start();
@@ -13,8 +30,16 @@ $id = $_SESSION['id'];
 
 
 $sql = "UPDATE produtos SET descricao = '$descricao', marca = '$marca', quantidade = $quantidade, preco = $preco WHERE codproduto = $codproduto";
-$query = $mysqli->query($sql) or die("Falha na execução do código SQL: " . $mysqli->error);
+// $query = $mysqli->query($sql) or die("Falha na execução do código SQL: " . $mysqli->error);
 
-header("Location: painel.php");
+if($mysqli->query($sql)){
+    echo '<div class="sucesso">Produto alterado com sucesso</div>';
+} else {
+    die("Falha na execução do código SQL: " . $mysqli->error);
+}
+
+// header("Location: painel.php");
 
 ?>
+
+<img src="imagens/imagemRodape2.png" alt="" id="rodape">
